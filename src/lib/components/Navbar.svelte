95d5d0ff55994e664c46d3ab104ab538f672086c
elianiva <dicha.arkana03@gmail.com>
2021-05-14 18:04:15 +0700

    feat:  improved navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -9,24 +9,40 @@
 	const pages = ['home', 'about', 'services', 'contact'];
 	let currentPage = '';
 
+	$: offsetY = 0;
+	$: isScrolledDown = offsetY >= 80;
+
 	onMount(() => {
 		const paths = $page.path.split('/');
 		currentPage = paths[paths.length - 1];
+
+		window.addEventListener('scroll', () => {
+			offsetY = window.pageYOffset;
+		});
 	});
 </script>
 
 <nav
-	class="fixed w-screen h-24 border-b-2 border-gray-200 border-dashed border-opacity-40"
+	class="fixed border-b-2 {isScrolledDown
+		? 'text-gray-700 bg-white'
+		: 'nav text-white border-dashed border-gray-200 border-opacity-40 '} transition-all duration-300 w-screen h-22 z-5"
 >
 	<div
-		class="max-w-3/4 grid grid-cols-3 justify-center items-center mx-auto h-full text-white"
+		class="max-w-screen-lg grid grid-cols-3 justify-center items-center mx-auto h-full text-white"
 	>
-		<h1 class="font-bold font-merriweather text-xl">HizirTransfer</h1>
+		<h1
+			class="font-bold font-lora text-xl {isScrolledDown
+				? 'text-gray-700'
+				: 'text-white'}"
+		>
+			HizirTransfer
+		</h1>
 		<div class="font-sans flex items-center justify-between">
 			{#each pages as r}
 				<a
-					class:text-yellow-300={currentPage == r}
-					class="hover:text-yellow-300 font-sm transition-color duration-100 ease-in"
+					class="font-sm transition-color duration-100 ease-in {isScrolledDown
+						? 'text-gray-700 hover:(text-amber-600 underline)'
+						: 'text-white hover:(text-amber-500 underline)'}"
 					href="/{r}"
 				>
 					{$_(`navbar.${r}`)}
@@ -34,15 +50,24 @@
 			{/each}
 		</div>
 		<div class="flex items-center justify-end gap-5">
-			<a href="https://facebook.com"><Facebook /></a>
-			<a href="https://twitter.com"><Twitter /></a>
-			<a href="https://whatsapp.com"><Whatsapp /></a>
+			<a
+				class={isScrolledDown ? 'text-gray-700' : 'text-white'}
+				href="https://facebook.com"><Facebook /></a
+			>
+			<a
+				class={isScrolledDown ? 'text-gray-700' : 'text-white'}
+				href="https://twitter.com"><Twitter /></a
+			>
+			<a
+				class={isScrolledDown ? 'text-gray-700' : 'text-white'}
+				href="https://whatsapp.com"><Whatsapp /></a
+			>
 		</div>
 	</div>
 </nav>
 
 <style>
-	nav {
+	.nav {
 		background-image: linear-gradient(
 			to top,
 			rgba(0, 0, 0, 0),
