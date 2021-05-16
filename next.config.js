d81b774be472a91aefc7aa4ada67d4b836984faa
elianiva <dicha.arkana03@gmail.com>
2021-05-16 08:40:39 +0700

    chore: init next and windicss

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin').default;
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new WindiCSSWebpackPlugin({
+        scan: {
+          dirs: ['./'],
+          exclude: ['node_modules', '.git', '.next/**/*'],
+        },
+      })
+    );
+    return config;
+  },
+};
