5693a3e485a0b1f807260561712aec7244b6e37a
elianiva <dicha.arkana03@gmail.com>
2021-05-16 19:28:23 +0700

    feat: add windicss config

---
+++ windi.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'windicss/helpers';
+import filters from 'windicss/plugin/filters';
+
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: ['Lora', 'serif'],
+        sans: ['Open Sans', 'serif'],
+      },
+    },
+  },
+  variants: {
+    filter: ['responsive'],
+  },
+  plugins: [filters],
+});
