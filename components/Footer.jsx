8eed6d30da8a5f7aeeb018462c7e39cbbabafc40
elianiva <dicha.arkana03@gmail.com>
2021-05-27 17:06:51 +0700

    feat: make footer responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,7 +20,7 @@ function ContactCard({ topLabel, bottomLabel, icon, className = '' }) {
 export default function Footer() {
   return (
     <footer
-      className="flex flex-col bg-gray-800 h-[20rem]"
+      className="flex flex-col bg-gray-800 min-h-[20rem]"
       style={{
         backgroundImage: 'url(/assets/world.png)',
         backgroundSize: 'contain',
@@ -30,29 +30,30 @@ export default function Footer() {
         <h1 className="font-medium text-2xl text-center text-white font-lora mb-10">
           HizirTransfer
         </h1>
-        <div className="grid grid-cols-4">
+        <div className="grid grid-rows-4 md:(grid-cols-4 grid-rows-1)">
           <ContactCard
             icon={<Phone width="24px" />}
             topLabel="0(550) 680-34-12"
             bottomLabel="Round-the-clock"
-            className="border-dashed border-r-2 border-gray-500 border-opacity-40"
+            className="border-dashed py-6 border-b-2 md:border-r-2 border-gray-500 border-opacity-40"
           />
           <ContactCard
             icon={<Location width="24px" />}
             topLabel="Somewhere In Turkey"
             bottomLabel="123456"
-            className="border-dashed border-r-2 border-gray-500 border-opacity-40"
+            className="border-dashed py-6 border-b-2 md:border-r-2 border-gray-500 border-opacity-40"
           />
           <ContactCard
             icon={<Mail width="24px" />}
             topLabel="info@hizirtransfer.com"
             bottomLabel="support@hizirtransfer.com"
-            className="border-dashed border-r-2 border-gray-500 border-opacity-40"
+            className="border-dashed py-6 border-b-2 md:border-r-2 border-gray-500 border-opacity-40"
           />
           <ContactCard
             icon={<Clock width="24px" />}
             topLabel="Opening Hours"
             bottomLabel="07:00 - 16:00"
+            className="py-6 md:py-0"
           />
         </div>
       </div>
